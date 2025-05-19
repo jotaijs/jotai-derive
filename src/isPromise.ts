40ef61824248a79ebd0e351a9b7d6f7eedee88c8
Iwo Plaza <iwoplaza@gmail.com>
2025-05-19 14:27:15 +0200

    soonAll knows less about ExtraPromise

--- src/isPromise.ts
+++ src/isPromise.ts
@@ -28,6 +28,28 @@ export function isPromise<T, S>(
 	return value && typeof value === 'object' && 'then' in value;
 }
 
+/**
+ * If it's a non promise, or a fulfilled promise.
+ */
+export function isKnown<T>(value: ExtraPromise<T> | unknown): boolean {
+	if (isPromise(value)) {
+		return value.status === 'fulfilled'; // only if fulfilled
+	}
+
+	return true; // not a promise, we know the value.
+}
+
+/**
+ * NOTE: If `promiseOrValue` is a Promise, but is not fulfilled, then it's undefined behavior.
+ * @returns `promiseOrValue` if it's not a Promise, the fulfilled value if it's a Promise.
+ */
+export function getFulfilledValue<T>(promiseOrValue: unknown): T {
+	if (isPromise(promiseOrValue)) {
+		return promiseOrValue.value as T;
+	}
+	return promiseOrValue as T;
+}
+
 export function getPromiseExtra<T>(
 	promise: unknown | Promise<T>,
 ): PromiseExtra<T> | undefined {
--- src/soonAll.ts
+++ src/soonAll.ts
@@ -1,18 +1,10 @@
-import { type ExtraPromise, isPromise } from './isPromise.js';
+import { getFulfilledValue, isKnown } from './isPromise.js';
 
 type PromiseOrValue<T> = Promise<T> | T;
 
 type SoonAll<T extends readonly unknown[]> = PromiseOrValue<{
 	[Index in keyof T]: Awaited<T[Index]>;
 }>;
-
-function isKnown<T, S>(value: ExtraPromise<T> | S): boolean {
-	if (isPromise(value)) {
-		return value.status === 'fulfilled'; // only if fulfilled
-	}
-
-	return true; // not a promise, we know the value.
-}
 
 /**
  * Given array `values`, if all elements are known (are not unresolved promises),
@@ -25,9 +17,7 @@ export function soonAll<T extends readonly unknown[] | []>(
 export function soonAll<T extends readonly unknown[]>(values: T): SoonAll<T>;
 export function soonAll<T extends readonly unknown[]>(values: T): SoonAll<T> {
 	if (values.every(isKnown)) {
-		return values.map((el) =>
-			isPromise(el) ? el.value : el,
-		) as unknown as SoonAll<T>;
+		return values.map((el) => getFulfilledValue(el)) as unknown as SoonAll<T>;
 	}
 
 	return Promise.all(values);
