d6af74f123f078e770a11da78e9ceafa3057a621
Iwo Plaza <iwoplaza@gmail.com>
2025-05-20 10:15:52 +0200

    Detect aborting in eagerAtom catch branch

--- packages/jotai-eager/src/eagerAtom.ts
+++ packages/jotai-eager/src/eagerAtom.ts
@@ -56,6 +56,9 @@ function resolveSuspension<T>(
           return resolveSuspension(compute, signal);
         },
         (reason) => {
+          if (signal.aborted) {
+            return undefined as T;
+          }
           setPromiseMeta(suspended, { status: 'rejected', reason });
           throw reason;
         },
